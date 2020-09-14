3f2e15f9382ac18403a97366397e181f2a981a63
KyleXu <kyle_x@foxmail.com>
2020-09-15 03:40:21 +0800

    [XTH]solve repeat code, build a core func

--- 4/src/main/java/com/twu/refactoring/NumberCruncher.java
+++ 4/src/main/java/com/twu/refactoring/NumberCruncher.java
@@ -2,40 +2,46 @@ package com.twu.refactoring;
 
 public class NumberCruncher {
     private final int[] numbers;
+    private String countType;
 
     public NumberCruncher(int... numbers) {
         this.numbers = numbers;
+        this.countType = "";
     }
 
     public int countEven() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) count++;
-        }
-        return count;
+        this.countType = "Even";
+        return countMethod(countType);
     }
 
     public int countOdd() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 1) count++;
-        }
-        return count;
+        this.countType = "Odd";
+        return countMethod(countType);
     }
 
     public int countPositive() {
+        this.countType = "Positive";
+        return countMethod(countType);
+    }
+
+    public int countNegative() {
+        this.countType = "negative";
+        return countMethod(countType);
+    }
+
+    public int countMethod(String countType) {
         int count = 0;
         for (int number : numbers) {
-            if (number >= 0) count++;
+            count = getCount(countType, count, "negative", number < 0);
+            count = getCount(countType, count, "Positive", number >= 0);
+            count = getCount(countType, count, "Odd", number %2 == 1);
+            count = getCount(countType, count, "Even", number %2 == 0);
         }
         return count;
     }
 
-    public int countNegative() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number < 0) count++;
-        }
+    private int getCount(String countType, int count, String type, boolean b) {
+        if (countType.equals(type) && b) count++;
         return count;
     }
 }
